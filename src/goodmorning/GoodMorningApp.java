df043010cdb568264bb471e74c571bc81806a983
ES\agarwad1 <dheeraj.agarwal@adp.com>
2020-03-31 19:56:09 +0530

    Good Morning App

---
+++ src/goodmorning/GoodMorningApp.java
@@ -0,0 +1,11 @@
+package goodmorning;
+
+public class GoodMorningApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good Morning!");
+
+	}
+
+}
