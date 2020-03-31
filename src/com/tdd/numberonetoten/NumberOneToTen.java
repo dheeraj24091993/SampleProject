147c900a0e9b050a9eece2f12b5c7949ee92d983
ES\agarwad1 <dheeraj.agarwal@adp.com>
2020-03-31 19:52:47 +0530

    Numbers 1 to 10

---
+++ src/com/tdd/numberonetoten/NumberOneToTen.java
@@ -0,0 +1,12 @@
+package com.tdd.numberonetoten;
+
+public class NumberOneToTen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1 ; i <= 10; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
