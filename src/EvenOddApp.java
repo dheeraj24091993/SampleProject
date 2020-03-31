e134e7a0c618ca1aabdfb209da36309586eb6b93
ES\agarwad1 <dheeraj.agarwal@adp.com>
2020-03-31 19:40:09 +0530

    Commiting Even Odd Code

---
+++ src/EvenOddApp.java
@@ -0,0 +1,16 @@
+
+public class EvenOddApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+		if(n%2==0) {
+			System.out.println("Number is even");
+		}
+		else {
+			System.out.println("Number is odd");
+		}
+
+	}
+
+}
